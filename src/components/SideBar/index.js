548b97d24044dc6827395cb3bc1e244d54bacf19
Mellie Dee <89175650+MellieDee@users.noreply.github.com>
2022-02-26 18:18:52 -0800

    create and styule sidebar component

---
+++ src/components/SideBar/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+const SideBar = () => {
+
+
+  return (
+    <>
+      <section className="my-5 px-3 side-bar">
+
+        <div className="my-2">
+          <p className='pb-4'>
+            Community. Strengthened online.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default SideBar
--- src/components/WhatDo/index.js
+++ src/components/WhatDo/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Hero from '../Hero'
+import SideBar from '../SideBar';
 // import coverImage from '../../assets/cover/cover-image.jpg';
 
 
@@ -12,15 +13,20 @@ const WhatDo = (props) => {
     <>
       <Hero></Hero>
       <section className="my-5 px-3 section-top">
-        <h3 className="section-title">I code for P<sup>3</sup></h3>
+        <h3 className="section-title">MDD for P<sup>3</sup></h3>
         <h4>Planet<span className="dot"></span>People<span className="dot"></span>Pets
         </h4>
         <div className="my-2">
           <p className='pb-4'>
-            I make the planet a better place for people and pets by building community. Increasingly, community is built online. I am your partner in your  development process, bringing your website designs - your community - to life.  Coding to increase your clicks, coding to increase your sales, and coding so you spend your time focusing on your mission. <br /> Together - we save the world. One website at a time.
+            Making the planet a better place for people and pets by building community. Your partner in development, bringing your website designs - and your community - to life.  <br />
+            <br />Coding to increase clicks. Coding to increase sales. Coding so you can focus on your mission. <br />
+            <br /> Together - we save the world. <br />
+
+            <br /> One website at a time.
           </p>
         </div>
       </section>
+      <SideBar></SideBar>
     </>
   );
 }
